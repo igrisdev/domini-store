4ae17850b47056552f782f2ed331a61e539ab706
igrisdev <johanalvarezz237@gmail.com>
2025-04-21 22:06:53 -0500

    feat: add checkout, card, details, products, and header components with basic structure and tests

---
+++ src/app/features/chechout/chechout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chechout',
+  imports: [],
+  templateUrl: './chechout.component.html',
+  styleUrl: './chechout.component.scss'
+})
+export class ChechoutComponent {
+
+}
